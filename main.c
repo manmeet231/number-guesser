08125199e1f5e1d573203ec6c59c077b72af07fe
manmeet singh <manmeetchahal231@gmail.com>
2024-06-15 10:15:00 +0530

    added game functionality

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <SDL2/SDL_ttf.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
 
 // Constants
 const int WINDOW_WIDTH = 800;
@@ -9,10 +12,11 @@ const int WINDOW_HEIGHT = 600;
 const int FONT_SIZE_ABOVE = 48;
 const int FONT_SIZE_BELOW = 24;
 const int MAX_INPUT_LENGTH = 100;
+const int FEEDBACK_DELAY_MS = 2000; // 2 seconds in milliseconds
 
 // Function prototypes
 void renderText(SDL_Renderer* renderer, const char* text, int x, int y, TTF_Font* font, SDL_Color color);
-
+void compareGuess(int guess, int randomNumber, char** message);
 
 int main(int argc, char* argv[]) {
     // Initialize SDL
@@ -27,6 +31,7 @@ int main(int argc, char* argv[]) {
         SDL_Quit();
         return 1;
     }
+
     // Create a window
     SDL_Window* window = SDL_CreateWindow("SDL2 Window",
                                           SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
@@ -48,12 +53,13 @@ int main(int argc, char* argv[]) {
         SDL_Quit();
         return 1;
     }
-    
+
     // Load a font for below text
     TTF_Font* font_below = TTF_OpenFont("assets/font.ttf", FONT_SIZE_BELOW);
-    if (font_above == NULL) {
+    if (font_below == NULL) {
         printf("Failed to load font! SDL_ttf Error: %s\n", TTF_GetError());
         SDL_DestroyWindow(window);
+        TTF_CloseFont(font_above);
         TTF_Quit();
         SDL_Quit();
         return 1;
@@ -65,6 +71,7 @@ int main(int argc, char* argv[]) {
         printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
         SDL_DestroyWindow(window);
         TTF_CloseFont(font_above);
+        TTF_CloseFont(font_below);
         TTF_Quit();
         SDL_Quit();
         return 1;
@@ -75,11 +82,22 @@ int main(int argc, char* argv[]) {
     memset(inputText, 0, sizeof(inputText)); // Initialize all elements to 0 (null terminator)
     SDL_StartTextInput();
 
+    // Seed the random number generator
+    srand(time(NULL));
+
+    // Generate a random number between 1 and 100
+    int randomNumber = rand() % 100 + 1;
+    char* message = "Start";
+    int userGuess = atoi(inputText);
+
+    // Flag to track if the user has guessed correctly
+    bool guessedCorrectly = false;
+
     // Event loop
     int quit = 0;
     SDL_Event e;
 
-    while (!quit) {
+    while (!quit && !guessedCorrectly) {
         while (SDL_PollEvent(&e) != 0) {
             if (e.type == SDL_QUIT) {
                 quit = 1;
@@ -93,7 +111,12 @@ int main(int argc, char* argv[]) {
                     inputText[strlen(inputText) - 1] = '\0';
                 } else if (e.key.keysym.sym == SDLK_RETURN) {
                     printf("User guessed: %s\n", inputText);
+                    int guess = atoi(inputText);
                     inputText[0] = '\0'; // Clear input text after guess
+                    compareGuess(guess, randomNumber, &message);
+                    if (strcmp(message, "correct") == 0) {
+                        guessedCorrectly = true;
+                    }
                 }
             }
         }
@@ -101,7 +124,10 @@ int main(int argc, char* argv[]) {
         // Clear screen
         SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
         SDL_RenderClear(renderer);
-        
+
+        SDL_Color textColor2 = { 0, 0, 0, 255 };
+        renderText(renderer, message, 200, 400, font_above, textColor2);
+
         // Render text above the input box
         SDL_Color textColor = { 0, 0, 0, 255 };
         renderText(renderer, "Enter Your Number!", 200, 100, font_above, textColor);
@@ -157,4 +183,20 @@ void renderText(SDL_Renderer* renderer, const char* text, int x, int y, TTF_Font
 
     SDL_FreeSurface(textSurface);
     SDL_DestroyTexture(textTexture); // Free the texture after use
+}
+
+// Function to compare the user's guess with the random number
+void compareGuess(int guess, int randomNumber, char** message) {
+    if (guess <100 && guess >0){
+        if (guess == randomNumber) {
+        *message = "correct";
+    } else if (guess < randomNumber) {
+        *message = "too low";
+    } else {
+        *message = "too high";
+    }
+    }
+    else{
+        *message = "invalid";
+    }
 }
